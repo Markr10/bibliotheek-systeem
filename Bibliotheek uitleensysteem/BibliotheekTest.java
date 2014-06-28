1db819b2ba2994a9f87b92d56704df495c90106e
Mark <mark-r@live.nl>
2014-06-28 22:46:12 +0200

    Methodes testAddLid en testAddBoek toegevoegd

--- Bibliotheek uitleensysteem/BibliotheekTest.java
+++ Bibliotheek uitleensysteem/BibliotheekTest.java
@@ -12,6 +12,9 @@ import org.junit.Test;
 public class BibliotheekTest
 {
     private Bibliotheek biblioth1;
+    private Bibliotheek biblioth2;
+
+    
     /**
      * Default constructor for test class BibliotheekTest.
      */
@@ -32,6 +35,7 @@ public class BibliotheekTest
         biblioth1.addBoek("Roman Empire", "Roman");
         biblioth1.addCd("Nederpop", "Pop", "12092006");
         biblioth1.addVideoband("The Bandit", "A");
+        biblioth2 = new Bibliotheek();
     }
 
     /**
@@ -43,5 +47,35 @@ public class BibliotheekTest
     public void tearDown()
     {
     }
+
+    @Test
+    public void testAddLid()
+    {
+        biblioth2.addLid("Paula");
+        biblioth2.addLid("Piet");
+        assertEquals(2, biblioth2.leden.size());
+        assertEquals("Paula", biblioth2.leden.get(0).getNaam());
+        assertEquals(0, biblioth2.leden.get(0).getID());
+        assertEquals("Piet", biblioth2.leden.get(1).getNaam());
+        assertEquals(1, biblioth2.leden.get(1).getID());
+    }
+
+    @Test
+    public void testAddBoek()
+    {
+        assertEquals(true, biblioth2.addBoek("Roman Empire", "Roman"));
+        assertEquals(false, biblioth2.addBoek("Roman Empire 2", "R"));
+        assertEquals(false, biblioth2.addBoek("E-learning", "S"));
+        assertEquals(true, biblioth2.addBoek("E-learning 2", "sTuDieBoEk"));
+        assertEquals(2, biblioth2.artikelen.size());
+        assertEquals("Roman Empire", biblioth2.artikelen.get(0).getTitel());
+        assertEquals(BoekType.valueOf("ROMAN"), biblioth2.artikelen.get(0).getType());
+        assertEquals(0, biblioth2.artikelen.get(0).getID());
+        assertEquals("E-learning 2", biblioth2.artikelen.get(1).getTitel());
+        assertEquals(BoekType.valueOf("STUDIEBOEK"), biblioth2.artikelen.get(1).getType());
+        assertEquals(1, biblioth2.artikelen.get(1).getID());
+    }
 }
+
+
 
