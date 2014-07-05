d2038a70d7fb591e7afa46c132ce96f0124f1711
Mark <mark-r@live.nl>
2014-07-05 19:14:02 +0200

    Throws exception instead of returning null

--- Bibliotheek uitleensysteem/ImportData.java
+++ Bibliotheek uitleensysteem/ImportData.java
@@ -18,58 +18,48 @@ public class ImportData
 
     /**
      * Import data from a CSV file and returns it as an ArrayList.
-     * The CSV file must start with the column names.
-     * The values must be separated with a comma.
+     * The CSV file must start with the column names. The values must be separated with a comma.
+     * It throws an exception when the data could not be imported.
      *
      * @param csvFilePath The path to the CSV file.
      * @param columns The colum name of the CSV file.
-     * @return Returns the import data from the CSV file. It returns null when the data could not be imported.
+     * @return Returns the import data from the CSV file.
      */
-    public static ArrayList<String[]> run(String csvFilePath, String[] columnNames) 
+    public static ArrayList<String[]> run(String csvFilePath, String[] columnNames) throws Exception
     {
         String csvSplitBy = ",";
-        
-        try 
+
+        BufferedReader br = new BufferedReader(new FileReader(csvFilePath));
+
+        // check correct start of the CSV file
+        String line = br.readLine(); // throws exception when there is no line
+        String[] firstSplittedLine = line.split(csvSplitBy);
+        for(int i = 0; i < firstSplittedLine.length; i++)
         {
-            BufferedReader br = new BufferedReader(new FileReader(csvFilePath));
-
-            // check correct start of the CSV file
-            String line = br.readLine(); // throws exception when there is no line
-            String[] firstSplittedLine = line.split(csvSplitBy);
-            for(int i = 0; i < firstSplittedLine.length; i++)
+            if(firstSplittedLine[i] != columnNames[i])
             {
-                if(firstSplittedLine[i] != columnNames[i])
-                {
-                    // not correct start of the CSV file
-                    throw new Exception("Start of CSV file is not correct!");
-                }
+                // not correct start of the CSV file
+                throw new Exception("Start of CSV file is not correct!");
             }
-
-            // import the rows with the data
-            ArrayList<String[]> importList = new ArrayList<String[]>();
-            while((line = br.readLine()) != null)
-            {
-                // split the line into the different columns
-                String[] splittedLine = line.split(csvSplitBy);
-                // check the number of columns
-                if(splittedLine.length != columnNames.length)
-                {
-                    throw new Exception("The number of the columns is not correct!\n" +
-                        "Corresponding line in CSV file: " + line);
-                }
-                importList.add(splittedLine);
-            }
-            br.close();
-
-            // the operation has completed successfully
-            return importList;
         }
-        catch (Exception e) 
+
+        // import the rows with the data
+        ArrayList<String[]> importList = new ArrayList<String[]>();
+        while((line = br.readLine()) != null)
         {
-            e.printStackTrace();
-
-            // the operation could not be successfully completed because there was an exception
-            return null;
+            // split the line into the different columns
+            String[] splittedLine = line.split(csvSplitBy);
+            // check the number of columns
+            if(splittedLine.length != columnNames.length)
+            {
+                throw new Exception("The number of the columns is not correct!\n" +
+                    "Corresponding line in CSV file: " + line);
+            }
+            importList.add(splittedLine);
         }
+        br.close();
+
+        // the operation has completed successfully
+        return importList;
     }
 }
