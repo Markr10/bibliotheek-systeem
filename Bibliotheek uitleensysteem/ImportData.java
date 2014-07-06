963a4b71654323c07c16a85c9f6ee059ad878503
Mark <mark-r@live.nl>
2014-07-07 00:49:43 +0200

    ImportData constructor is nu standaard niet meer bereikbaar, commentaar geüpdatet, exceptions verbeterd en IncorrectStartCsvFileException toegevoegd, bugs geplet

--- Bibliotheek uitleensysteem/ImportData.java
+++ Bibliotheek uitleensysteem/ImportData.java
@@ -13,16 +13,21 @@ public class ImportData
 {
     /**
      * ImportData constructor
+     * The constructor is private because this is a static class.
      */
-    public ImportData() {}
+    private ImportData()
+    {
+    }
 
     /**
      * Import data from a CSV file and returns it as an ArrayList.
-     * The CSV file must start with the column names. The (column) values must be separated with a comma.
+     * The CSV file must start with the column names.
+     * The (column) values must be separated with a comma.
      * It throws an exception when the data could not be imported.
      *
      * @param csvFilePath The path to the CSV file.
-     * @param columns The colum names of the CSV file.
+     * @param columns The colum names of the CSV file. A column name must consist of one or
+     * more characters.
      * @return Returns the import data from the CSV file.
      */
     public static ArrayList<String[]> run(String csvFilePath, String[] columnNames) throws Exception
@@ -33,13 +38,23 @@ public class ImportData
 
         // check correct start of the CSV file
         String line = br.readLine(); // throws exception when there is no line
-        String[] firstSplittedLine = line.split(csvSplitBy);
-        for(int i = 0; i < firstSplittedLine.length; i++)
+        String[] firstSplittedLine = line.split(csvSplitBy, -1);
+        if(columnNames.length != firstSplittedLine.length)
         {
-            if(firstSplittedLine[i] != columnNames[i])
+                throw new IncorrectStartCsvFileException("The number of the columns names of the CSV file is not correct!");
+        }
+        for(int i = 0; i < columnNames.length; i++)
+        {
+            if(columnNames[i].isEmpty())
+            {
+                // column name is not correct
+                throw new IncorrectStartCsvFileException("A column name must consist of one or more characters!");
+            }
+            if(!columnNames[i].equals(firstSplittedLine[i]))
             {
                 // not correct start of the CSV file
-                throw new Exception("Start of CSV file is not correct!");
+                throw new IncorrectStartCsvFileException("The expected column name \"" + columnNames[i] + "\" " +
+                    "is not equals to the founded column name \"" + firstSplittedLine[i] + "\"");
             }
         }
 
@@ -48,12 +63,12 @@ public class ImportData
         while((line = br.readLine()) != null)
         {
             // split the line into the different columns
-            String[] splittedLine = line.split(csvSplitBy);
+            String[] splittedLine = line.split(csvSplitBy, -1);
             // check the number of columns
             if(splittedLine.length != columnNames.length)
             {
-                throw new Exception("The number of the columns is not correct!\n" +
-                    "Corresponding line in CSV file: " + line);
+                throw new Exception("The number of the columns is not correct! " +
+                    "Corresponding line in CSV file: \"" + line + "\"");
             }
             importList.add(splittedLine);
         }
